ff4b9da2bae28f3d3cf1fdb536299b00a7d828b0
Gordon Grant-Stuart <gordon.grant-stuart@opsview.com>
2022-02-18 10:51:05 +0000

    neatening it

--- passivecheck.sh
+++ passivecheck.sh
@@ -46,14 +46,6 @@ else
   exit
 fi
 
-function urlencode() {
-  echo -n "$1" | sed "s/%/%25/g; s/ /%20/g; s/\!/%21/g; s/\"/%22/g; s/\#/%23/g; s/\\$/%24/g;
-   s/\&/%26/g; s/'/%27/g; s/(/%28/g; s/)/%29/g; s/*/%2A/g; s/\+/%2B/g; s/\,/%2C/g; s/-/%2D/g;
-   s/\./%2E/g; s/\//%2F/g; s/\:/%3A/g; s/\;/%3B/g; s/</%3C/g; s/\=/%3D/g; s/>/%3E/g; s/\?/%3F/g;
-   s/\@/%40/g; s/\[/%5B/g; s/\\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/\_/%5F/g; s/\`/%60/g; s/{/%7B/g;
-   s/|/%7C/g; s/\}/%7D/g; s/\~/%7E/g;" | tr '\n' '#' | sed 's/#/%0A/g'
-}
-
 errfile=$(mktemp /tmp/err.XXX)
 perfdata="$(eval "$metric_command" 2>$errfile)"
 ret=$?
@@ -75,6 +67,14 @@ else
   comment=$(cat $errfile)
 fi
 rm -f $errfile
+
+function urlencode() {
+  echo -n "$1" | sed "s/%/%25/g; s/ /%20/g; s/\!/%21/g; s/\"/%22/g; s/\#/%23/g; s/\\$/%24/g;
+   s/\&/%26/g; s/'/%27/g; s/(/%28/g; s/)/%29/g; s/*/%2A/g; s/\+/%2B/g; s/\,/%2C/g; s/-/%2D/g;
+   s/\./%2E/g; s/\//%2F/g; s/\:/%3A/g; s/\;/%3B/g; s/</%3C/g; s/\=/%3D/g; s/>/%3E/g; s/\?/%3F/g;
+   s/\@/%40/g; s/\[/%5B/g; s/\\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/\_/%5F/g; s/\`/%60/g; s/{/%7B/g;
+   s/|/%7C/g; s/\}/%7D/g; s/\~/%7E/g;" | tr '\n' '#' | sed 's/#/%0A/g'
+}
 
 checkdata="servicename=$(urlencode "$servicename")&hostname=$(urlencode "$hostname")&new_state=$(urlencode "$new_state")&comment=$(urlencode "$comment")|$(urlencode "$perfdata")"
 
